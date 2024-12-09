2d1cf7d346594cc77be573003a69ae48a716f812
mcflugen <mcflugen@gmail.com>
2024-12-09 16:01:58 -0700

    insert doctest code for plot.py

--- deltametrics/plot.py
+++ deltametrics/plot.py
@@ -1122,8 +1122,18 @@ def show_one_dimensional_trajectory_to_strata(
     For example, we can quickly visualize the processing of a 1D timeseries of
     bed elevations into boxy stratigraphy with this routine.
 
-    .. plot:: guides/userguide_1d_example.py
-        :include-source:
+    .. plot::
+
+        >>> import matplotlib.pyplot as plt
+        >>> from deltametrics.plot import show_one_dimensional_trajectory_to_strata
+        >>> from deltametrics.sample_data import golf
+
+        >>> golfcube = golf()
+
+        >>> ets = golfcube['eta'].data[:, 10, 85]  # a "real" slice of the model
+
+        >>> fig, ax = plt.subplots(figsize=(8, 4))
+        >>> show_one_dimensional_trajectory_to_strata(ets, ax=ax, dz=0.25)
 
     The orange line depicts the resultant stratigraphy, with all
     bed-elevations above this line cut from the stratigraphy by the
