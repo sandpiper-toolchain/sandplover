8559d7b02349e3b726ce0ff4567b72a5464937ee
mcflugen <mcflugen@gmail.com>
2024-12-10 12:40:06 -0700

    reorder imports for the docs

--- docs/source/pyplots/examples/preserved_velocities.py
+++ docs/source/pyplots/examples/preserved_velocities.py
@@ -1,6 +1,7 @@
-import numpy as np
-import deltametrics as dm
 import matplotlib.pyplot as plt
+import numpy as np
+
+import deltametrics as dm
 
 golfcube = dm.sample_data.golf()
 golfcube.stratigraphy_from('eta')
