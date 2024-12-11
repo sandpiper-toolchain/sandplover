f1988943e7b473351b888e850b174a9a8b81c309
mcflugen <mcflugen@users.noreply.github.com>
2024-12-11 04:48:37 +0000

    Deploying to gh-pages from @ DeltaRCM/DeltaMetrics@de772b2985c9f3069b41ec633b28132dbe4e7868 🚀

--- _downloads/2007985b5904f1d220df9007aa21f554/index-2.py
+++ _downloads/2007985b5904f1d220df9007aa21f554/index-2.py
@@ -1,18 +1,20 @@
-xslope0, xslope1 = dm.sample_data.xslope()
+import matplotlib.pyplot as plt
+import numpy as np
+from deltametrics.sample_data.sample_data import xslope
+#
+xslope0, xslope1 = xslope()
 nt = 5
-ts = np.linspace(0, xslope0['eta'].shape[0]-1, num=nt, dtype=int)
-
+ts = np.linspace(0, xslope0["eta"].shape[0] - 1, num=nt, dtype=int)
+#
 fig, ax = plt.subplots(2, nt, figsize=(12, 2))
 for i, t in enumerate(ts):
-    ax[0, i].imshow(xslope0['eta'][t, :, :], vmin=-10, vmax=0.5)
-    ax[0, i].set_title('t = ' + str(t))
-    ax[1, i].imshow(xslope1['eta'][t, :, :], vmin=-10, vmax=0.5)
-
-ax[1, 0].set_ylabel('dim1 direction')
-ax[1, 0].set_xlabel('dim2 direction')
-
+    _ = ax[0, i].imshow(xslope0["eta"][t, :, :], vmin=-10, vmax=0.5)
+    _ = ax[0, i].set_title(f"t = {t}")
+    _ = ax[1, i].imshow(xslope1["eta"][t, :, :], vmin=-10, vmax=0.5)
+#
+_ = ax[1, 0].set_ylabel("dim1 direction")
+_ = ax[1, 0].set_xlabel("dim2 direction")
+#
 for axi in ax.ravel():
-    axi.axes.get_xaxis().set_ticks([])
-    axi.axes.get_yaxis().set_ticks([])
-
-plt.show()
+    _ = axi.axes.get_xaxis().set_ticks([])
+    _ = axi.axes.get_yaxis().set_ticks([])
