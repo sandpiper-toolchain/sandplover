a45b361b8709ffde25d42487af3ef18fc1afaead
mcflugen <mcflugen@gmail.com>
2024-12-18 15:56:49 -0700

    fix SIM102 in utils module

--- deltametrics/utils.py
+++ deltametrics/utils.py
@@ -518,13 +518,15 @@ def _point_in_polygon(x, y, polygon):
     p1x, p1y = polygon[0]
     for i in range(n+1):
         p2x, p2y = polygon[i % n]
-        if y > min(p1y, p2y):
-            if y <= max(p1y, p2y):
-                if x <= max(p1x, p2x):
-                    if p1y != p2y:
-                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                    if p1x == p2x or x <= xints:
-                        inside = not inside
+        if (
+            (y > min(p1y, p2y))
+            and (y <= max(p1y, p2y))
+            and (x <= max(p1x, p2x))
+        ):
+            if p1y != p2y:
+                xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+            if p1x == p2x or x <= xints:
+                inside = not inside
         p1x, p1y = p2x, p2y
 
     return inside
