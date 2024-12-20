faef48a31e7c51e14e12866ed5cfa9e80e67365e
mcflugen <mcflugen@gmail.com>
2024-12-20 10:19:26 -0700

    blacken the pyplots in the docs

--- docs/source/pyplots/mask/centerlinemask.py
+++ docs/source/pyplots/mask/centerlinemask.py
@@ -1,10 +1,12 @@
 """Visual for CenterlineMask."""
+
 import deltametrics as dm
 from deltametrics.mask import CenterlineMask
 from deltametrics.mask import ChannelMask
 
 golfcube = dm.sample_data.golf()
-channel_mask = ChannelMask(golfcube['velocity'].data[-1, :, :],
-                           golfcube['eta'].data[-1, :, :])
+channel_mask = ChannelMask(
+    golfcube["velocity"].data[-1, :, :], golfcube["eta"].data[-1, :, :]
+)
 centerline_mask = CenterlineMask(channel_mask)
 centerline_mask.show()
--- docs/source/pyplots/mask/edgemask.py
+++ docs/source/pyplots/mask/edgemask.py
@@ -1,7 +1,8 @@
 """Visual for EdgeMask."""
+
 import deltametrics as dm
 from deltametrics.mask import EdgeMask
 
 golfcube = dm.sample_data.golf()
-edge_mask = EdgeMask(golfcube['eta'].data[-1, :, :])
+edge_mask = EdgeMask(golfcube["eta"].data[-1, :, :])
 edge_mask.show()
